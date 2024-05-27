88e7be6a496d987b6278433c919332eb512705a4
iAmLeO25 <bikramghalley@gmail.com>
2024-05-27 14:59:07 +0600

    added backend Node.js

---
+++ Backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+// it will take json data as input from requests/frontend
+app.use(express.json());
+app.use(cors());
+
+const categoryRoute = require('./modules/category/router');
+
+app.use('/category', categoryRoute);
+
+app.get('/', (request, response) => {
+    response.send('API is running');
+});
+
+app.listen(8000, (req, res) => {
+    console.log('Server is running on port 8000');
+    mongoose.connect(
+        "mongodb+srv://admin:admin@atlascluster.wsab61r.mongodb.net/",
+    ).then((_) => {
+        console.log('Connected to DB');
+    }).catch(err => {
+        console.log(err);
+    });
+});
+
---
+++ Backend/utils/constants/category.constants.js
@@ -0,0 +1,17 @@
+const CATEGORY = [
+
+    {id: 1, title: "Food "},
+    {id: 2, title: "Clothing "},
+    {id: 3, title: "Loan "},
+    {id: 4, title: "Personal Expenses "},
+    {id: 5, title: "Vehicle "},
+    {id: 6, title: "Grocries "},
+    {id: 7, title: "Electronics "},
+    {id: 8, title: "Title 8"},
+    {id: 9, title: "Title 9"},
+    {id: 10, title: "Title 10"},
+];
+
+module.exports = {
+    CATEGORY
+}
